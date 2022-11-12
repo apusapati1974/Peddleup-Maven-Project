e4dca62b84b562dc806035b7a6e9c03c4c7d9cad
DELL <DELL@DESKTOP-HR0BL1L>
2022-11-12 10:38:35 +0530

    ClientLoginPage - TestNG

--- PeddleUpTest/src/test/java/com/peddleup/qa/testcases/ClientLoginPageTest.java
+++ PeddleUpTest/src/test/java/com/peddleup/qa/testcases/ClientLoginPageTest.java
@@ -1,6 +1,7 @@
 package com.peddleup.qa.testcases;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
