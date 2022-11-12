7a50175fb3d19ec0f2952a282fdfabec74f68a4d
DELL <DELL@DESKTOP-HR0BL1L>
2022-11-12 10:14:57 +0530

    Client LoginPage

--- PeddleUpTest/src/main/java/com/peddleup/qa/pages/ClientLoginPage.java
+++ PeddleUpTest/src/main/java/com/peddleup/qa/pages/ClientLoginPage.java
@@ -1,9 +1,15 @@
 package com.peddleup.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.peddleup.qa.base.TestBase;
 
 public class ClientLoginPage extends TestBase{
 	
 	// First we have to define PageFactory / Object Repository
-	
+	// First we have to define Object Repository / Page factory.
+		@FindBy(name = "email_id")
+		WebElement email_id;
+
 }
