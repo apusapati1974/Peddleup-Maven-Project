297304315bdd9e0b3f8a47c2062f6088f5862253
DELL <DELL@DESKTOP-HR0BL1L>
2022-11-12 10:36:21 +0530

    ClientLoginPage

--- PeddleUpTest/src/main/java/com/peddleup/qa/pages/ClientLoginPage.java
+++ PeddleUpTest/src/main/java/com/peddleup/qa/pages/ClientLoginPage.java
@@ -2,14 +2,54 @@ package com.peddleup.qa.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import com.peddleup.qa.base.TestBase;
 
 public class ClientLoginPage extends TestBase{
-	
+
 	// First we have to define PageFactory / Object Repository
-	// First we have to define Object Repository / Page factory.
-		@FindBy(name = "email_id")
-		WebElement email_id;
+
+	@FindBy(name = "email_id")
+	WebElement email_id;
+
+	@FindBy(name = "password")
+	WebElement password;
+
+	@FindBy(xpath = "//label[contains(text(),'Agency')]")
+	WebElement Agency;
+
+	@FindBy(xpath = "//button[contains(text(),'LOGIN')]")
+	WebElement loginbtn;
+
+	/*
+	 *	Creating constructor of base class
+	 *	driver is came from the base class TestBase.java, 
+	 *	this means pointing to current class objects.
+	 *	This is initializing the Page Objects
+	 */ 
+
+	public ClientLoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	// Validating the page title 
+	public String validateLoginPageTitle() {
+		return driver.getTitle();
+	}
+
+
+	public ClientManageProfilePage login(String email,String pwd) throws InterruptedException {
+		email_id.sendKeys(email);
+		Thread.sleep(2000);
+		password.sendKeys(pwd);
+		Thread.sleep(2000);
+		Agency.click();
+		Thread.sleep(2000);
+		loginbtn.click();
+
+		// should return ManageProfilePage Oject.
+		return new ClientManageProfilePage();
+	}
 
 }
